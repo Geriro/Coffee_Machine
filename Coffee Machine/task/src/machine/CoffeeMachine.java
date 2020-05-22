79b24efccd24b36d60c86b8a8fd5f89225fc6b27
Enzo SEKKAI <gtegto@yahoo.fr>
2020-05-22 21:15:03 +0200

    test 4 passé

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -5,35 +5,110 @@ import java.util.Scanner;
 public class CoffeeMachine {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int machine[] = {400, 540, 120, 9, 550};
 
+        etat(machine);
 
-        System.out.println("Write how many ml of water the coffee machine has:");
-        int water = scanner.nextInt();
+        System.out.println("Write action (buy, fill, take):");
+        String action = getAction(scanner);
 
-        System.out.println("Write how many ml of milk the coffee machine has:");
-        int milk = scanner.nextInt();
+        switch (action) {
+            case "buy":
+                System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:");
+                int drink = getDrink(scanner);
 
-        System.out.println("Write how many grams of coffee beans the coffee machine has:");
-        int beans = scanner.nextInt();
+                switch (drink) {
+                    case 1:
+                        machine = doDrink(machine, 250, 0, 16, 4);
+                        break;
 
-        System.out.println("Write how many cups of coffee you will need:");
-        int cupsN = scanner.nextInt();
+                    case 2:
+                        machine = doDrink(machine, 350, 75, 20, 7);
+                        break;
 
-        int cupW = water / 200;
-        int cupM = milk / 50;
-        int cupB = beans / 15;
+                    case 3:
+                        machine = doDrink(machine, 200, 100, 12, 6);
+                        break;
 
-        int cupsP = cupW < cupM ? (cupW < cupB ? cupW : cupB) : (cupM < cupB ? cupM : cupB);
+                    default:
+                        System.out.println("Not a drink");
+                }
+                break;
 
-        if (cupsP == cupsN) {
-            System.out.println("Yes, I can make that amount of coffee");
-        } else if (cupsP > cupsN) {
-            System.out.println("Yes, I can make that amount of coffee (and even " + (cupsP - cupsN) + "more than that)");
-        } else {
-            System.out.println("No, I can make only " + cupsP + "cup(s) of coffee");
+            case "fill":
+                System.out.println("Write how many ml of water do you want to add:");
+                int waterF = scanner.nextInt();
+                System.out.println("Write how many ml of milk do you want to add:");
+                int milkF = scanner.nextInt();
+                System.out.println("Write how many grams of coffee beans do you want to add:");
+                int beansF = scanner.nextInt();
+                System.out.println("Write how many disposable cups of coffee do you want to add:");
+                int cupsF = scanner.nextInt();
+                machine = fill(machine, waterF, milkF, beansF, cupsF);
+                break;
+
+            case "take":
+                machine = take(machine);
+                break;
+
+            default:
+                System.out.println("Not a good action");
         }
 
-
-
+        etat(machine);
     }
+
+    private static void etat (int[] list) {
+        System.out.println("The coffee machine has:");
+        System.out.println(list[0] + " of water");
+        System.out.println(list[1] + " of milk");
+        System.out.println(list[2] + " of coffee beans");
+        System.out.println(list[3] + " of disposable cups");
+        System.out.println(list[4] + " of money");
+    }
+
+    private static String getAction(Scanner scanner) {
+        return scanner.nextLine();
+    }
+
+    private static int getDrink(Scanner scanner) {
+        return scanner.nextInt();
+    }
+
+    private static int[] doDrink(int[] list, int waterp, int milkp, int beansp, int moneyp) {
+        int[] listRes = list;
+
+        list[0] -= waterp;
+        list[1] -= milkp;
+        list[2] -= beansp;
+        list[3] -= 1;
+        list[4] += moneyp;
+
+        return listRes;
+    }
+
+    private static int[] fill(int[] list, int waterp, int milkp, int beansp, int cupsp) {
+        int[] listRes = list;
+
+        list[0] += waterp;
+        list[1] += milkp;
+        list[2] += beansp;
+        list[3] += cupsp;
+
+        return listRes;
+    }
+
+    private static int[] take(int[] list) {
+        int[] listRes = list;
+
+        list[4] = 0;
+
+        return listRes;
+    }
+
+
+
+
+
+
 }
